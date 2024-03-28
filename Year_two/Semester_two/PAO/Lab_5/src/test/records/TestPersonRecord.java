4173f1758b10ff11e5bd905c33b2c68af8275fe2
alexandramocanu1 <119112644+alexandramocanu1@users.noreply.github.com>
2024-03-28 17:37:36 +0200

    Add files via upload

---
+++ Year_two/Semester_two/PAO/Lab_5/src/test/records/TestPersonRecord.java
@@ -0,0 +1,12 @@
+package test.compare;
+
+import records.PersonRecord;
+
+public class TestPersonRecord {
+    public static void main(String[] args) {
+        PersonRecord user = new PersonRecord("Maria", 30);
+        System.out.println(user.name());
+        System.out.println(user.age());
+        System.out.println(user);
+    }
+}
